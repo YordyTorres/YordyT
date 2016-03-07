90d3dc9d4bebd39ce64b679ef034a0718204322f
YordyTorres <yordy.ytc.torres@Gmail.com>
2016-03-07 11:42:21 -0500

    Added files via upload

---
+++ MoveMe.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveMe : MonoBehaviour {
+
+public	Vector3 speed = new Vector3 (0f, 0f, 0f);
+	public GameObject[] moveables;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (GetComponent<Transform>().position);
+
+
+        // instead of doing GameObject.GetComponent<Transform>
+        //GetComponent<Transform> ().position += speed;
+        //transform.Translate (speed);
+
+        foreach (GameObject thingToMove in moveables)
+        {
+            thingToMove.transform.Translate(speed);
+        }    
+            foreach (GameObject thingToMove in moveables) {
+            thingToMove.transform.position = new Vector3(Mathf.PingPong(Time.time, 3), transform.position.y, transform.position.z);
+
+        }
+
+
+    }
+}
---
+++ pattern.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pattern : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.PingPong(Time.time, 3), transform.position.y, transform.position.z);
+    }
+}
